72e222141349202f8db41e2ab6da35103ce6de0b
森祐一朗 <apotikisin4632@gmail.com>
2024-04-20 12:03:42 +0900

    ツイート一覧機能作成

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -3,11 +3,18 @@
 module Api
   module V1
     class TweetsController < ApplicationController
-      before_action :authenticate_api_v1_user!, only: %i[create]
+      before_action :authenticate_api_v1_user!, only: %i[index create]
+      def index
+        page = (params[:offset].to_i / params[:limit].to_i) + 1
+        tweets = Tweet.page(page).per(params[:limit]).order(created_at: 'DESC')
+        total_page = tweets.total_pages
+        render json: { tweet: tweets, total_page: }, include: [:user], status: :ok, methods: [:image_urls]
+      end
+
       def create
         tweet = current_api_v1_user.tweets.build(tweet_param)
         if tweet.save
-          render json: { tweet: }, status: :ok
+          render json: { tweet: }, status: :ok, methods: [:image_urls]
         else
           render json: { tweet: tweet.errors }, status: :unprocessable_entity
         end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,15 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
+  include Rails.application.routes.url_helpers
+
   has_many_attached :images
 
   validates :content, presence: true, length: { maximum: 140 }
+
+  def image_urls
+    return unless images.attached?
+
+    images.map { |image| url_for(image) }
+  end
 end
