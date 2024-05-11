81e657e42423b5e463927f08620c4a08741c8483
森祐一朗 <apotikisin4632@gmail.com>
2024-05-11 15:27:58 +0900

    プロフィール画面のコメント一覧実装に伴う修正

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -8,7 +8,7 @@ module Api
       def show
         user = User.find_by(id: params[:id])
         render json: { user: }, status: :ok, methods: %i[profile_image_url header_image_url],
-               include: { tweets: { methods: :image_urls } }
+               include: { tweets: { methods: :image_urls }, comments: {} }
       end
 
       def update
